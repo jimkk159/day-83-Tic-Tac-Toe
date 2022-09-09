aa962c8814b8797d4b94804330f0f8ef4fe1f782
Jim <jimkk159@gmail.com>
2022-09-10 01:15:33 +0800

    color blink and game end continue

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 
 
-class tic_tac_toe:
+class TicTacToe:
 
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('TIC TAC TOE')
+        self.blink_color = [black, white]
         self.choice = (0, 0)
         self.clock = pygame.time.Clock()
+        self.count = 0
         self.FPS = 60
         self.map = []
         self.wel_type = 0
@@ -21,6 +23,9 @@ class tic_tac_toe:
         self.turn = 'Human'
         self.windows = pygame.display.set_mode((X, Y))
         self.welcome()
+
+    def reset_map(self):
+        self.map = [['', '', ''], ['', '', ''], ['', '', '']]
 
     def welcome(self):
         self.windows.fill(black)
@@ -38,17 +43,17 @@ class tic_tac_toe:
 
         font = pygame.font.Font('freesansbold.ttf', 28)
         if self.wel_type == 0:
-            text = font.render('Enter', True, black, white)
+            text = font.render('Enter', True, self.blink_color[0], self.blink_color[1])
         else:
             text = font.render('Enter', True, white, black)
         enter_text = text.get_rect()
         enter_text.midleft = (X // 2 - 40, Y // 2 + 30)
         self.windows.blit(text, enter_text)
 
-        if self.wel_type == 0:
-            text = font.render('Exit', True, white, black)
+        if self.wel_type == 1:
+            text = font.render('Exit', True, self.blink_color[0], self.blink_color[1])
         else:
-            text = font.render('Exit', True, black, white)
+            text = font.render('Exit', True, white, black)
 
         exit_text = text.get_rect()
         exit_text.midleft = (X // 2 - 40, Y // 2 + 80)
@@ -56,8 +61,7 @@ class tic_tac_toe:
 
     def ending(self):
         pygame.draw.rect(self.windows, black,
-                         pygame.Rect(X // 2-100, Y // 2-30, 200, 60))
-        pygame.display.flip()
+                         pygame.Rect(X // 2 - 100, Y // 2 - 30, 200, 60))
         font = pygame.font.Font('freesansbold.ttf', 44)
         if self.turn == 'Human':
             text = font.render('You Win!', True, white, black)
@@ -65,6 +69,14 @@ class tic_tac_toe:
             text = font.render('You Lose!', True, white, black)
         ending_text = text.get_rect()
         ending_text.center = (X // 2, Y // 2)
+        self.windows.blit(text, ending_text)
+
+        pygame.draw.rect(self.windows, black,
+                         pygame.Rect(X // 2 - 120, Y // 2 + 35, 240, 30))
+        font = pygame.font.Font('freesansbold.ttf', 20)
+        text = font.render('Press Enter to continue', True, self.blink_color[0], black)
+        ending_text = text.get_rect()
+        ending_text.center = (X // 2, Y // 2 + 50)
         self.windows.blit(text, ending_text)
 
     def is_end(self):
@@ -75,13 +87,16 @@ class tic_tac_toe:
                 self.situation = 'end'
 
         for col in range(cols):
-            if self.map[::][col].count('O') == 3 or self.map[:][col].count('X') == 3:
+            column = [item[col] for item in self.map]
+            if column.count('O') == 3 or column.count('X') == 3:
                 self.situation = 'end'
 
-        if self.map[0][0] != '' and self.map[0][0] == self.map[1][1] == self.map[2][2]:
+        if (self.map[1][1] == 'O' or self.map[1][1] == 'X') and \
+                (self.map[0][0] == self.map[1][1] == self.map[2][2] or
+                 self.map[0][2] == self.map[1][1] == self.map[2][0]):
             self.situation = 'end'
 
-    def game_body(self):
+    def game_board(self):
         self.windows.fill(black)
         self.draw_title(X - 90, 20)
         self.draw_dash_line(X // 2, Y // 2 + 50)
@@ -106,20 +121,19 @@ class tic_tac_toe:
             for col in range(cols):
                 color = white
                 if self.choice == (row, col):
-                    color = black
+                    color = self.blink_color[0]
                 if self.map[row][col] != '':
                     symbol = self.map[row][col]
                     font = pygame.font.Font('freesansbold.ttf', 70)
-                    text = font.render(symbol, True, color)
+                    text = font.render(symbol, True, color, )
                     symbol = text.get_rect()
                     symbol.center = (X // 2 + (col - 1) * 100, Y // 2 + (row - 1) * 100 + 5)
                     self.windows.blit(text, symbol)
 
     def draw_choice(self):
         row, col = self.choice
-        pygame.draw.rect(self.windows, white,
+        pygame.draw.rect(self.windows, self.blink_color[1],
                          pygame.Rect(X // 2 - 40 + (col - 1) * 100, Y // 2 - 37 + (row - 1) * 100, 80, 80))
-        pygame.display.flip()
 
     def draw_dash_line(self, x, y):
         bottom_line = '- ' * 12
@@ -138,6 +152,14 @@ class tic_tac_toe:
             horizontal_line.center = (x, y - 125 + item * 50)
             self.windows.blit(text, horizontal_line)
 
+    def color_blink(self):
+        self.count += 1
+        if self.count == 50:
+            self.blink_color = (self.blink_color[1], self.blink_color[0])
+
+        if self.count == 60:
+            self.count = 0
+
     def event_maintain(self):
 
         self.clock.tick(self.FPS)
@@ -152,17 +174,19 @@ class tic_tac_toe:
                 if self.situation == 'init':
 
                     if key_in[pygame.K_UP]:
+                        self.count = 0
+                        self.blink_color = [black, white]
                         self.wel_type = 0
-                        self.welcome()
 
                     if key_in[pygame.K_DOWN]:
+                        self.blink_color = [black, white]
+                        self.count = 0
                         self.wel_type = 1
-                        self.welcome()
 
                     if key_in[pygame.K_RETURN]:
                         if self.wel_type == 0:
                             self.situation = 'body'
-                            self.map = [['', '', ''], ['', 'O', ''], ['', 'X', '']]
+                            self.map = [['X', '', ''], ['X', '', ''], ['X', '', '']]
 
                         elif self.wel_type == 1:
                             pygame.quit()
@@ -177,21 +201,25 @@ class tic_tac_toe:
                             if row < 0:
                                 row = 1
                             self.choice = row, col
+
                         elif key_in[pygame.K_DOWN]:
                             row += 1
                             if row > 2:
                                 row = 2
                             self.choice = row, col
+
                         elif key_in[pygame.K_LEFT]:
                             col -= 1
                             if col < 0:
                                 col = 0
                             self.choice = row, col
+
                         elif key_in[pygame.K_RIGHT]:
                             col += 1
                             if col > 2:
                                 col = 2
                             self.choice = row, col
+
                         elif key_in[pygame.K_RETURN]:
 
                             row, col = self.choice
@@ -199,6 +227,11 @@ class tic_tac_toe:
                                 self.map[row][col] = 'O'
                                 # self.turn = 'AI'
 
+                elif self.situation == 'end':
+                    print(1)
+                    if key_in[pygame.K_RETURN]:
+                        self.situation = 'body'
+                        self.reset_map()
                     # elif self.turn == 'AI':
                     #     rows = len(self.map)
                     #     cols = len(self.map[0])
@@ -212,18 +245,22 @@ class tic_tac_toe:
                     #             for row in rows:
                     #                 ai_choice = (row, col)
 
-                if self.situation == 'body':
-                    self.game_body()
-                    self.is_end()
+        if self.situation == 'init':
+            self.welcome()
 
-                if self.situation == 'end':
-                    self.ending()
+        if self.situation == 'body':
+            self.game_board()
+            self.is_end()
 
-                pygame.display.update()
+        if self.situation == 'end':
+            self.ending()
+
+        self.color_blink()
+        pygame.display.update()
 
 
 if __name__ == '__main__':
-    game = tic_tac_toe()
+    game = TicTacToe()
     while True:
         game.event_maintain()
         pygame.display.update()
