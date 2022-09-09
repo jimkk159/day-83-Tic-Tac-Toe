4f3214e917c117bfc07f595c4cd54f384e01e858
Jim <jimkk159@gmail.com>
2022-09-09 22:45:31 +0800

    build game end

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ class tic_tac_toe:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('TIC TAC TOE')
-        self.FPS = 60
-        self.wel_type = 0
-        self.situation = 'init'
-        self.map = []
         self.choice = (0, 0)
         self.clock = pygame.time.Clock()
+        self.FPS = 60
+        self.map = []
+        self.wel_type = 0
+        self.situation = 'init'
+        self.turn = 'Human'
         self.windows = pygame.display.set_mode((X, Y))
         self.welcome()
 
@@ -52,6 +53,33 @@ class tic_tac_toe:
         exit_text = text.get_rect()
         exit_text.midleft = (X // 2 - 40, Y // 2 + 80)
         self.windows.blit(text, exit_text)
+
+    def ending(self):
+        pygame.draw.rect(self.windows, black,
+                         pygame.Rect(X // 2-100, Y // 2-30, 200, 60))
+        pygame.display.flip()
+        font = pygame.font.Font('freesansbold.ttf', 44)
+        if self.turn == 'Human':
+            text = font.render('You Win!', True, white, black)
+        else:
+            text = font.render('You Lose!', True, white, black)
+        ending_text = text.get_rect()
+        ending_text.center = (X // 2, Y // 2)
+        self.windows.blit(text, ending_text)
+
+    def is_end(self):
+        rows = len(self.map)
+        cols = len(self.map[0])
+        for row in range(rows):
+            if self.map[row].count('O') == 3 or self.map[row].count('X') == 3:
+                self.situation = 'end'
+
+        for col in range(cols):
+            if self.map[::][col].count('O') == 3 or self.map[:][col].count('X') == 3:
+                self.situation = 'end'
+
+        if self.map[0][0] != '' and self.map[0][0] == self.map[1][1] == self.map[2][2]:
+            self.situation = 'end'
 
     def game_body(self):
         self.windows.fill(black)
@@ -134,43 +162,64 @@ class tic_tac_toe:
                     if key_in[pygame.K_RETURN]:
                         if self.wel_type == 0:
                             self.situation = 'body'
-                            self.map = [['', '', ''], ['', '', ''], ['', '', '']]
+                            self.map = [['', '', ''], ['', 'O', ''], ['', 'X', '']]
 
                         elif self.wel_type == 1:
                             pygame.quit()
                             quit()
 
                 elif self.situation == 'body':
-                    row, col = self.choice
-                    if key_in[pygame.K_UP]:
-                        row -= 1
-                        if row < 0:
-                            row = 1
-                        self.choice = row, col
-                    elif key_in[pygame.K_DOWN]:
-                        row += 1
-                        if row > 2:
-                            row = 2
-                        self.choice = row, col
-                    elif key_in[pygame.K_LEFT]:
-                        col -= 1
-                        if col < 0:
-                            col = 0
-                        self.choice = row, col
-                    elif key_in[pygame.K_RIGHT]:
-                        col += 1
-                        if col > 2:
-                            col = 2
-                        self.choice = row, col
-                    elif key_in[pygame.K_RETURN]:
 
+                    if self.turn == 'Human':
                         row, col = self.choice
-                        if self.map[row][col] == '':
-                            self.map[row][col] = 'O'
+                        if key_in[pygame.K_UP]:
+                            row -= 1
+                            if row < 0:
+                                row = 1
+                            self.choice = row, col
+                        elif key_in[pygame.K_DOWN]:
+                            row += 1
+                            if row > 2:
+                                row = 2
+                            self.choice = row, col
+                        elif key_in[pygame.K_LEFT]:
+                            col -= 1
+                            if col < 0:
+                                col = 0
+                            self.choice = row, col
+                        elif key_in[pygame.K_RIGHT]:
+                            col += 1
+                            if col > 2:
+                                col = 2
+                            self.choice = row, col
+                        elif key_in[pygame.K_RETURN]:
+
+                            row, col = self.choice
+                            if self.map[row][col] == '':
+                                self.map[row][col] = 'O'
+                                # self.turn = 'AI'
+
+                    # elif self.turn == 'AI':
+                    #     rows = len(self.map)
+                    #     cols = len(self.map[0])
+                    #     # defend
+                    #     for row in rows:
+                    #         if self.map[row].count('O') == 2:
+                    #             for col in cols:
+                    #                 ai_choice = (row, col)
+                    #     for col in cols:
+                    #         if self.map[row].count('O') == 2:
+                    #             for row in rows:
+                    #                 ai_choice = (row, col)
 
                 if self.situation == 'body':
                     self.game_body()
-                    pygame.display.update()
+                    self.is_end()
+
+                if self.situation == 'end':
+                    self.ending()
+
+                pygame.display.update()
 
 
 if __name__ == '__main__':
